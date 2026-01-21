cb9cd04a5ef7135c298f4294f4c45954b49a5e19
Reinis Valters <166013545+23DP3RValt@users.noreply.github.com>
2026-01-21 09:21:40 +0200

    initial commit

---
+++ backend/example-app/vite.config.ts
@@ -0,0 +1,29 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import vuetify from 'vite-plugin-vuetify';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        wayfinder({
+            formVariants: true,
+        }),
+        vuetify({ autoImport: true }),
+    ],
+});
